b959aa69523de64a4cb25ed85597a2eb789d85e8
Kalpana Swaminathan <kalpana294@gmail.com>
2017-12-20 21:11:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object , key,value){
   return Object.assign({}, object, {[key]: value});
 }
-/*function updateObjectWithKeyAndValue(object,key,value){
-  var newObj = Object.assign({},obj)
-  newObj.key = value;
- return newObj 
-}*/
+
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object[key] = value;
   return object
