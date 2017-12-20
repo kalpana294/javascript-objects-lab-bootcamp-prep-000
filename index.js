f4f6aad22df081e2d426deb34a5dea77985b9b8d
Kalpana Swaminathan <kalpana294@gmail.com>
2017-12-20 21:14:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 }
 
 function deleteFromObjectByKey(object,key){
-  delete object.key
+  var newObject = Object.assign({},object)
+  delete newObject.key
   return object
 }
