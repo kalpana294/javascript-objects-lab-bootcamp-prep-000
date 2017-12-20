451e1f35102be23b03b0b8c81cafe89dce2a5a1a
Kalpana Swaminathan <kalpana294@gmail.com>
2017-12-20 20:59:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function updateObjectWithKeyAndValue(obj,key,value){
  return newObj 
 }
 function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
-  obj.key = value
+  obj["key"] = value
   return obj
 }
